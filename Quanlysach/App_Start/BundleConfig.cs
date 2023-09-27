e26689e501ce07e7abc2a228f6d1beba286e7db5
Duy's <khanhduytran0508@gmail.com>
2023-09-27 11:29:48 +0700

    Layout

--- Quanlysach/App_Start/BundleConfig.cs
+++ Quanlysach/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Quanlysach
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/myStyles.css"));
         }
     }
 }
