13bb3ff5793cdf49199ff0f22397c27848bd9a7a
tumai213 <115773868+tumai213@users.noreply.github.com>
2023-10-11 16:35:36 +0700

    kết nối database

--- Quanlysach/App_Start/RouteConfig.cs
+++ Quanlysach/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Quanlysach
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Bookstore", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Quanlysach/Controllers/BookstoreController.cs
@@ -0,0 +1,35 @@
+﻿using Quanlysach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quanlysach.Controllers
+{
+    public class BookstoreController : Controller
+    {
+        QLBANSACHEntities1 database= new QLBANSACHEntities1();
+        private List<SACH> LaySachMoi(int soluong)
+        {
+            //Sx theo ngày cập nhập giảm dần, lấy theo biến soluong. Chuyen sang dang danh sach
+            return database.SACHes.OrderByDescending(sach => sach.Ngaycapnhat).Take(soluong).ToList();
+        }
+        // GET: Bookstore
+        public ActionResult Index()
+        {
+            var dsSachMoi = LaySachMoi(13);
+            return View(dsSachMoi);
+        }
+        public ActionResult LayChuDe()
+        {
+            var dsChuDe = database.CHUDEs.ToList();
+            return PartialView(dsChuDe);
+        }
+        public ActionResult LayNhaXuatBan()
+        {
+            var dsNhaXB = database.NHAXUATBANs.ToList();
+            return PartialView(dsNhaXB);
+        }
+    }
+}
